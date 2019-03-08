4512f1281e62153ea48f0ce24e11267ef33e745c
oscarde1 <47445045+oscarde1@users.noreply.github.com>
2019-03-08 09:33:43 +0100

    Add files via upload

---
+++ crud.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <style>
+            table{
+                border: 1px solid black;
+            }
+
+            th, td {
+                border: 1px solid black;
+            }
+        </style>
+    </head>
+    <body>
+
+        <?php
+        include 'modelo/Peliculas.php';
+        include 'modelo/Cds.php';
+
+        if (isset($_POST['opcion'])) {
+
+            $opcion = $_POST['opcion'];
+
+            switch ($opcion) {
+                case "libros":
+
+
+                    break;
+                case "cds":
+
+                    consultaCDS();
+
+                    break;
+
+                case "peliculas":
+
+                    consultaPelis();
+
+                    break;
+                default:
+                    break;
+            }
+        }
+        ?>
+
+        <form  method="post" action="crud.php">
+            <input type="submit" name="opcion" value="libros">
+            <input type="submit" name="opcion" value="cds">
+            <input type="submit" name="opcion" value="peliculas">
+        </form>
+    </body>
+</html>
